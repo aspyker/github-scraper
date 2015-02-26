4c78ed9a4526aac8047ee39e03cbdd87c183b9cb
Andrew Spyker <awspyker@gmail.com>
2015-02-25 18:11:03 -0800

    initial drop

---
+++ build.sbt
@@ -0,0 +1,22 @@
+name := """github-scraper"""
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
+fork := true
+
+libraryDependencies ++= {
+  val scalaTestVersion = "2.1.6"
+  val newmanVersion = "1.3.5"
+  val logbackVersion = "1.1.2"
+  val playJsonVersion = "2.4.0-M2"
+  val kohsukeGithubVersion = "1.62"
+  Seq(
+    "org.scalatest" %% "scalatest" % scalaTestVersion % "test",
+    "com.stackmob" %% "newman" % newmanVersion,
+    "ch.qos.logback" % "logback-classic" % logbackVersion,
+    "com.typesafe.play" % "play-json_2.10" % playJsonVersion,
+    "org.kohsuke" % "github-api" %   kohsukeGithubVersion
+  )
+}
---
+++ src/main/scala/com/netflix/aspyker/tools/github/QueryGitHub.scala
@@ -0,0 +1,106 @@
+package com.netflix.aspyker.tools.github
+
+import com.stackmob.newman._
+import com.stackmob.newman.dsl._
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.net.URL
+import org.slf4j.LoggerFactory
+import play.api.libs.json._
+import org.kohsuke.github._
+import scala.collection.JavaConversions._
+import java.util.Date
+
+object QueryGitHub {
+  def logger = LoggerFactory.getLogger(getClass)
+  def githubOrg = "netflix"
+
+  def main(args: Array[String]): Unit = {
+    val github = GitHub.connect()
+    val orgs = github.getOrganization(githubOrg)
+    val repos = orgs.listRepositories(100).asList()
+    
+    val (privateRepos, publicRepos) = repos.partition { repo => repo.isPrivate() }
+    walkRepos(publicRepos.toList)
+    System.exit(0)
+  }
+  
+  def walkRepos(repos: List[GHRepository]) : Unit = {
+    val reposJsonSeq = repos.map(repo => {
+      logger.info(s"repo = ${repo.getName()}, forks = ${repo.getForks}, stars = ${repo.getWatchers}")
+      val openPullRequests = repo.getPullRequests(GHIssueState.OPEN)
+      logger.debug(s"  openIssues = ${repo.getOpenIssueCount()}, openPullRequests = ${openPullRequests.size()}")
+      
+      // TODO: Is there a faster way to only pull the last commit?
+      val commits = repo.listCommits().asList()
+      val orderedCommits = commits.sortBy(_.getCommitShortInfo.getCommitter().getDate())
+      val lastCommitDate = orderedCommits(orderedCommits.length - 1).getCommitShortInfo().getCommitter().getDate()
+      //logger.debug(s"commits, first = ${orderedCommits(0).getSHA1}, last = ${orderedCommits(orderedCommits.length - 1).getSHA1()}")
+      val daysSinceLastCommit = daysBetween(lastCommitDate, new Date())
+      logger.debug(s"  daysSinceLastCommit = ${daysSinceLastCommit}")
+      
+      val contributors = commits.filter { commit => Option(commit.getAuthor()).isDefined }
+      val contributorEmails = contributors.map(contributor => contributor.getAuthor().getLogin()).distinct
+      logger.debug(s"  numContribitors = ${contributorEmails.length}, contributorEmails = ${contributorEmails}")
+      
+      val closedIssues = repo.getIssues(GHIssueState.CLOSED)
+      val timeToCloseIssue = closedIssues.map(issue => {
+        val opened = issue.getCreatedAt()
+        val closed = issue.getClosedAt()
+        val difference = daysBetween(opened, closed)
+        difference
+      })
+      val sumIssues = timeToCloseIssue.sum
+      val avgIssues = timeToCloseIssue.size match {
+        case 0 => 0
+        case _ => sumIssues / timeToCloseIssue.size
+      }
+      logger.debug(s"    avg days to close ${closedIssues.size()} issues = ${avgIssues} days")
+     
+      // TODO: Look at refactoring with above into function
+      val closedPRs = repo.getPullRequests(GHIssueState.CLOSED)
+      val timeToClosePR = closedPRs.map(pr => {
+        val opened = pr.getCreatedAt()
+        val closed = pr.getClosedAt()
+        val difference = daysBetween(opened, closed)
+        difference
+      })
+      val sumPRs = timeToClosePR.sum
+      val avgPRs = timeToClosePR.size match {
+        case 0 => 0
+        case _ => sumPRs / timeToClosePR.size
+      }
+      logger.debug(s"    avg days to close ${closedPRs.size()} pull requests = ${avgPRs} days")
+      
+      val repoJson: JsValue = Json.obj(
+        "name" -> repo.getName(),
+        "forks" -> repo.getForks(),
+        "stars" -> repo.getWatchers(),
+        "numContributors" -> daysSinceLastCommit,
+        "issues" -> Json.obj(
+            "openCount" -> repo.getOpenIssueCount(),
+            "closedCount" -> closedIssues.size(),
+            "avgTimeToCloseInDays" -> avgIssues
+        ),
+        "pullRequests" -> Json.obj(
+            "openCount" -> openPullRequests.size(),
+            "closedCount" -> closedPRs.size(),
+            "avgTimeToCloseInDays" -> avgPRs
+        ),
+        "commits" -> Json.obj(
+            "daysSinceLastCommit" -> daysSinceLastCommit
+        ),
+        "contributors" -> contributorEmails.toSeq
+      )
+      logger.debug("repo json = " + repoJson)
+      repoJson
+    })
+    val allRepos = Json.obj("asOf" -> new Date().toGMTString(), "repos" -> reposJsonSeq)
+    logger.info(s"reposJson = ${allRepos}")
+  }
+  
+  def daysBetween(smaller: Date, bigger: Date): Int = {
+    val diff = (bigger.getTime() - smaller.getTime()) / (1000 * 60 * 60 * 24)
+    diff.toInt
+  }
+}
