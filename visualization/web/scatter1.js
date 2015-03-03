e2a5fd24a3fa404937b1607f0430815872b498c2
Andrew Spyker <awspyker@gmail.com>
2015-03-02 21:22:18 -0800

    combine charts, add zoom, add more charts

--- visualization/web/scatter1.js
+++ visualization/web/scatter1.js
@@ -1,41 +1,56 @@
-var loadeddata = null;
+var loadeddata = {};
 
-nv.addGraph(function() {
-  var chart = nv.models.scatterChart().color(d3.scale.category10().range());
-  chart.xAxis.axisLabel("forks");
-  chart.yAxis.axisLabel("stars");
-  chart.showLegend(false);
+function removeGraphs() {
+  var svgs = d3.selectAll("svg > *");
+  svgs.remove();
+}
 
-  chart.tooltipContent(function(groupName, x, y) {
-	  console.log(groupName);
-	  console.log(x);
-	  console.log(y);
-	  for (ii = 0; ii < loadeddata.length; ii++) {
-		  if (loadeddata[ii].x == x && loadeddata[ii].y == y) {
-			  return loadeddata[ii].label + "(" + loadeddata[ii].numContributors + " contributors)";
+function addGraph(divname, xAxisLabel, yAxisLabel, jsonRelUrl, bubbleTTfirst, bubbleTTlast, bubbleSizeField, xMax, yMax) {
+	if (arguments.length == 7) {
+		xMax = -1;
+		yMax = -1;
+	}
+	
+	addGraphFunction = function(xMax, yMax, divname) {
+	  var chart = nv.models.scatterChart().color(d3.scale.category10().range());
+	  chart.xAxis.axisLabel(xAxisLabel);
+	  chart.yAxis.axisLabel(yAxisLabel);
+	  chart.showLegend(false);
+	
+	  chart.tooltipContent(function(groupName, x, y) {
+		  //console.log(groupName);
+		  //console.log(x);
+		  //console.log(y);
+		  for (ii = 0; ii < loadeddata[divname].length; ii++) {
+			  if (loadeddata[divname][ii].x == x && loadeddata[divname][ii].y == y) {
+				  // TODO: Look into string interpolation and/or templating long term
+				  return loadeddata[divname][ii].label + bubbleTTfirst + loadeddata[divname][ii].bubbleTTmiddle + bubbleTTlast;
+			  }
 		  }
-	  }
-	  return "unknown";
-  });
+		  return "unknown";
+	  });
+	
+	  var myData = myRealData(jsonRelUrl, bubbleSizeField, xMax, yMax, divname);
+		  
+	  d3.select('#' + divname + ' svg')
+	      .datum(myData)
+	      .call(chart);
+	
+	  nv.utils.windowResize(chart.update);
+	
+	  return chart;
+	};
+	
+	nv.addGraph(addGraphFunction(xMax, yMax, divname));
+}
 
-  var myData = myRealData();
-	  
-  d3.select('#chart svg')
-      .datum(myData)
-      .call(chart);
-
-  nv.utils.windowResize(chart.update);
-
-  return chart;
-});
-
-function myRealData() {
+function myRealData(jsonRelUrl, bubbleSizeField, xMax, yMax, divname) {
 	var mydata = null;
 	var data = [];
 	var random = d3.random.normal();
 	
 	$.ajax({
-		url: 'asOf-20150225.json', 
+		url: jsonRelUrl, 
 		async: false,
 		dataType: 'json',
 		success: function(data) {
@@ -46,22 +61,41 @@ function myRealData() {
     	key: 'Group 0',
     	values: []
 	});
-    loadeddata = []
+    loadeddata[divname] = []
     for (ii = 0; ii < mydata.repos.length; ii++) {
-    	loadeddata.push({
+    	if (xMax != -1) {
+    		var xVal = mydata.repos[ii].forks;
+    		var yVal = mydata.repos[ii].stars;
+    		if (xVal > xMax || yVal > yMax) {
+    			continue; // don't plot the point
+    		}
+    	}
+    	size = accessProperty(mydata.repos[ii], bubbleSizeField);
+    	console.log('bubble size field = ' + size)
+    	var dataToLoad = {
     		label: mydata.repos[ii].name,
-    		x: mydata.repos[ii].forks,
-    		y: mydata.repos[ii].stars,
-    		numContributors: mydata.repos[ii].numContributors
-    	})
+        	x: mydata.repos[ii].forks,
+        	y: mydata.repos[ii].stars,
+        	bubbleTTmiddle: size
+        };
+    	console.log(dataToLoad)
+    	loadeddata[divname].push(dataToLoad)
     	data[0].values.push({
     		label: mydata.repos[ii].name,
     		x: mydata.repos[ii].forks,
     		y: mydata.repos[ii].stars,
-    		size: mydata.repos[ii].numContributors,
+    		size: size * 1000,
     		shape: "circle"
         });
     }
 
 	return data;
+}
+
+function accessProperty (object, string){
+	var explodedString = string.split('.');
+	for (i = 0, l = explodedString.length; i<l; i++){
+		object = object[explodedString[i]];
+	}
+	return object;
 }
